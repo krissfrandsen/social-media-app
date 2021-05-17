dc2d558f53f5cd76a6441a4b06bfd74da8ca0853
Kriss <krissfrandsen@gmail.com>
2021-05-18 00:17:53 +0200

    set-up webpack

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # social-media-app
+
+This a responsive social media app using React, react hooks, styled components, MongoDB,
---
+++ app/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Main = () => {
+  return <div></div>;
+};
+
+ReactDOM.render(<Main />, document.querySelector("#app"));
