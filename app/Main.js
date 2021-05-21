420be5970001d7203aeb10ad69de99f1d1dcbc2d
Kriss <krissfrandsen@gmail.com>
2021-05-21 12:21:17 +0200

    add backbone component

--- app/Main.js
+++ app/Main.js
@@ -1,8 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us" exact>
+          <About />
+        </Route>
+        <Route path="/terms" exact>
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 ReactDOM.render(<Main />, document.querySelector("#app"));
+
+if (module.hot) {
+  module.hot.accept();
+}
